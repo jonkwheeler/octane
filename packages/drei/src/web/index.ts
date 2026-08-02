5f94802f3dfb7cccec4b5cc7b22d282fe7c08161
Jon K. Wheeler <jon.k.wheeler@gmail.com>
2026-08-02 06:28:05 -0400

    feat(drei): port cycle raycast

--- packages/drei/src/web/index.ts
+++ packages/drei/src/web/index.ts
@@ -1,3 +1,4 @@
 export { useCursor } from './useCursor.three.tsrx';
 export { KeyboardControls, useKeyboardControls } from './KeyboardControls.three.tsrx';
 export type { KeyboardControlsEntry, KeyboardControlsProps } from './KeyboardControls.three.tsrx';
+export { CycleRaycast } from './CycleRaycast.three.tsrx';
