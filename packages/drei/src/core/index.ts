104fe36252430b6ceeb81ddc5171088109e57886
Jon K. Wheeler <jon.k.wheeler@gmail.com>
2026-08-02 06:06:53 -0400

    feat(drei): port object helpers

--- packages/drei/src/core/index.ts
+++ packages/drei/src/core/index.ts
@@ -118,6 +118,8 @@ export { Shadow, ShadowAlpha } from './shadows.three.tsrx';
 export type { ShadowAlphaProps, ShadowProps, ShadowType } from './shadows.three.tsrx';
 export { Grid } from './Grid.three.tsrx';
 export type { GridMaterialType, GridProps } from './Grid.three.tsrx';
+export { Helper, useHelper } from './Helper.three.tsrx';
+export type { HelperProps } from './Helper.three.tsrx';
 export type {
 	ArcballControlsProps,
 	DeviceOrientationControlsProps,
