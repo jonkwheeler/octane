c6f053914f25a036e54092483ead6e13a916acd1
Jon K. Wheeler <jon.k.wheeler@gmail.com>
2026-08-02 14:57:47 -0400

    feat(input-otp): define public types and utilities

---
+++ packages/input-otp/src/index.ts
@@ -0,0 +1,7 @@
+export { OTPInput, OTPInputContext } from './input.tsrx';
+export {
+	REGEXP_ONLY_CHARS,
+	REGEXP_ONLY_DIGITS,
+	REGEXP_ONLY_DIGITS_AND_CHARS,
+} from './regexp';
+export type { OTPInputProps, RenderProps, SlotProps } from './types';
