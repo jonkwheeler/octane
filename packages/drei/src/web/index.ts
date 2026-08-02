b3b62895efdad4a96ac91cc3901750ad325fcf00
Jon K. Wheeler <jon.k.wheeler@gmail.com>
2026-08-02 06:24:23 -0400

    feat(drei): port keyboard controls

--- packages/drei/src/web/index.ts
+++ packages/drei/src/web/index.ts
@@ -1,1 +1,3 @@
 export { useCursor } from './useCursor.three.tsrx';
+export { KeyboardControls, useKeyboardControls } from './KeyboardControls.three.tsrx';
+export type { KeyboardControlsEntry, KeyboardControlsProps } from './KeyboardControls.three.tsrx';
