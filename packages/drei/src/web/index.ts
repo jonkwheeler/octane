c404bc7b382462eed05dfd85e78c424b00b56f3d
Jon K. Wheeler <jon.k.wheeler@gmail.com>
2026-08-02 06:39:17 -0400

    feat(drei): port selection controls

--- packages/drei/src/web/index.ts
+++ packages/drei/src/web/index.ts
@@ -2,3 +2,5 @@ export { useCursor } from './useCursor.three.tsrx';
 export { KeyboardControls, useKeyboardControls } from './KeyboardControls.three.tsrx';
 export type { KeyboardControlsEntry, KeyboardControlsProps } from './KeyboardControls.three.tsrx';
 export { CycleRaycast } from './CycleRaycast.three.tsrx';
+export { Select, useSelect } from './Select.three.tsrx';
+export type { SelectProps } from './Select.three.tsrx';
